10f7b86fe016af6f81cfef50c6874bbf19cd61f8
Daniel Deady <daniel@clockworkgeek.com>
2017-02-15 19:26:16 +0000

    Make languages column nice

--- code/Block/Adminhtml/Request/Grid.php
+++ code/Block/Adminhtml/Request/Grid.php
@@ -14,7 +14,7 @@ class Capita_TI_Block_Adminhtml_Request_Grid extends Mage_Adminhtml_Block_Widget
 
 	protected function _prepareCollection()
 	{
-		$collection = Mage::getResourceModel('admin/user_collection');
+		$collection = Mage::getResourceModel('capita_ti/request_collection');
 		$this->setCollection($collection);
 		return parent::_prepareCollection();
 	}
@@ -22,16 +22,17 @@ class Capita_TI_Block_Adminhtml_Request_Grid extends Mage_Adminhtml_Block_Widget
     protected function _prepareColumns()
     {
 		$this->addColumn('request_id', array(
-			'index' => 'user_id',
+			'index' => 'request_id',
 			'header' => $this->__('ID'),
 		    'type' => 'number',
 			'width' => '100px'
 		));
-		$this->addColumn('languages', array(
-			'index' => 'languages',
+		$this->addColumn('dest_language', array(
+			'index' => 'dest_language',
 			'header' => $this->__('Languages'),
 		    'type' => 'options',
-		    'options' => Mage::getSingleton('capita_ti/api_languages')->getLanguagesInUse()
+		    'options' => Mage::getSingleton('capita_ti/api_languages')->getLanguagesInUse(),
+		    'filter_condition_callback' => array($this, 'filterLanguages')
 		));
 		$this->addColumn('delivery_date', array(
 			'index' => 'delivery_date',
@@ -50,4 +51,8 @@ class Capita_TI_Block_Adminhtml_Request_Grid extends Mage_Adminhtml_Block_Widget
 		return parent::_prepareColumns();
     }
 
+    public function filterLanguages(Capita_TI_Model_Resource_Request_Collection $collection, Mage_Adminhtml_Block_Widget_Grid_Column $column)
+    {
+        $collection->addFilterLikeLanguage($column->getFilter()->getValue());
+    }
 }
--- code/Model/Resource/Request/Collection.php
+++ code/Model/Resource/Request/Collection.php
@@ -7,4 +7,23 @@ class Capita_TI_Model_Resource_Request_Collection extends Mage_Core_Model_Resour
     {
         $this->_init('capita_ti/request');
     }
+
+    protected function _afterLoad()
+    {
+        foreach ($this as $request) {
+            if ($request->hasDestLanguage()) {
+                $request->setDestLanguage(
+                    explode(',', $request->getDestLanguage())
+                );
+            }
+        }
+    }
+
+    public function addFilterLikeLanguage($language)
+    {
+        // addFieldToFilter escapes values for us
+        $this->addFieldToFilter(
+            'dest_language',
+            array('like' => '%'.$language.'%'));
+    }
 }
