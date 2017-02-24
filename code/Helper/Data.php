1eb5370b00623ccc0cddc8ac83586edd8a891bdc
Daniel Deady <daniel@clockworkgeek.com>
2017-02-24 17:35:05 +0000

    Remove unused `getProductAttributesOptions` function

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -35,29 +35,4 @@ class Capita_TI_Helper_Data extends Mage_Core_Helper_Data
         $languages = Mage::getSingleton('capita_ti/api_languages')->getLanguagesInUse();
         return $this->convertHashToOptions($languages);
     }
-
-    /**
-     * Get translateable attributes in options/values format
-     * 
-     * @return array
-     */
-    public function getProductAttributesOptions()
-    {
-        /* @var $attributes Mage_Catalog_Model_Resource_Product_Attribute_Collection */
-        $attributes = Mage::getResourceModel('catalog/product_attribute_collection');
-        $attributes->addFieldToFilter('main_table.frontend_input', array('in' => array('text', 'textarea')));
-        $attributes->addFieldToFilter('main_table.backend_type', array('in' => array('text', 'varchar')));
-        $attributes->addFieldToFilter('main_table.attribute_code', array('nin' => array('created_at', 'custom_layout_update', 'has_options', 'required_options')));
-
-        $options = array();
-        /* @var $attribute Mage_Eav_Model_Entity_Attribute */
-        foreach ($attributes as $attribute) {
-            $options[] = array(
-                'value' => $attribute->getAttributeCode(),
-                'label' => $attribute->getFrontend()->getLabel()
-            );
-        }
-        return $options;
-    }
-
 }
