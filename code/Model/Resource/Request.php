f46d67976fa73163b0e01df934ba5fae5db5d114
Daniel Deady <daniel@clockworkgeek.com>
2017-03-21 11:04:28 +0000

    Consolidate several similar save functions

--- code/Model/Resource/Request.php
+++ code/Model/Resource/Request.php
@@ -106,23 +106,23 @@ class Capita_TI_Model_Resource_Request extends Mage_Core_Model_Resource_Db_Abstr
         }
 
         if ($request->dataHasChangedFor('product_ids')) {
-            $this->_saveProducts($request);
+            $this->_saveLinks($request, 'capita_ti/product', 'product_id');
         }
 
         if ($request->dataHasChangedFor('category_ids')) {
-            $this->_saveCategories($request);
+            $this->_saveLinks($request, 'capita_ti/category', 'category_id');
         }
 
         if ($request->dataHasChangedFor('block_ids')) {
-            $this->_saveBlocks($request);
+            $this->_saveLinks($request, 'capita_ti/block', 'block_id');
         }
 
         if ($request->dataHasChangedFor('page_ids')) {
-            $this->_savePages($request);
+            $this->_saveLinks($request, 'capita_ti/page', 'page_id');
         }
 
         if ($request->dataHasChangedFor('attribute_ids')) {
-            $this->_saveAttributes($request);
+            $this->_saveLinks($request, 'capita_ti/attribute', 'attribute_id');
         }
 
         return parent::_afterSave($request);
@@ -158,133 +158,30 @@ class Capita_TI_Model_Resource_Request extends Mage_Core_Model_Resource_Db_Abstr
             ->setOrigData('documents', $documents);
     }
 
-    protected function _saveProducts(Capita_TI_Model_Request $request)
+    protected function _saveLinks(Capita_TI_Model_Request $request, $tableEntity, $idFieldName)
     {
-        $productTable = $this->getTable('capita_ti/product');
-        $productIds = $request->getProductIds();
-        if (!is_array($productIds)) {
-            $productIds = explode(',', (string) $productIds);
+        $linkTable = $this->getTable($tableEntity);
+        $idsFieldName = $idFieldName . 's';
+        $entityIds = $request->getData($idsFieldName);
+        if (!is_array($entityIds)) {
+            $entityIds = explode(',', (string) $entityIds);
         }
-
+        
         $adapter = $this->_getWriteAdapter();
         $condition = sprintf(
             '(%s) AND (%s)',
             $adapter->prepareSqlCondition('request_id', $request->getId()),
-            $adapter->prepareSqlCondition('product_id', array('nin' => $productIds)));
-        $adapter->delete($productTable, $condition);
-
+            $adapter->prepareSqlCondition($idFieldName, array('nin' => $entityIds)));
+        $adapter->delete($linkTable, $condition);
+        
         $insertData = array();
-        foreach ($productIds as $productId) {
+        foreach ($entityIds as $entityId) {
             $insertData[] = array(
                 'request_id' => $request->getId(),
-                'product_id' => $productId
+                $idFieldName => $entityId
             );
         }
-        $adapter->insertOnDuplicate($productTable, $insertData);
-        $request->setOrigData('product_ids', $productIds);
-    }
-
-    protected function _saveCategories(Capita_TI_Model_Request $request)
-    {
-        $categoryTable = $this->getTable('capita_ti/category');
-        $categoryIds = $request->getCategoryIds();
-        if (!is_array($categoryIds)) {
-            $categoryIds = explode(',', (string) $categoryIds);
-        }
-
-        $adapter = $this->_getWriteAdapter();
-        $condition = sprintf(
-            '(%s) AND (%s)',
-            $adapter->prepareSqlCondition('request_id', $request->getId()),
-            $adapter->prepareSqlCondition('category_id', array('nin' => $categoryIds)));
-        $adapter->delete($categoryTable, $condition);
-
-        $insertData = array();
-        foreach ($categoryIds as $categoryId) {
-            $insertData[] = array(
-                'request_id' => $request->getId(),
-                'category_id' => $categoryId
-            );
-        }
-        $adapter->insertOnDuplicate($categoryTable, $insertData);
-        $request->setOrigData('category_ids', $categoryIds);
-    }
-
-    protected function _saveBlocks(Capita_TI_Model_Request $request)
-    {
-        $blockTable = $this->getTable('capita_ti/block');
-        $blockIds = $request->getBlockIds();
-        if (!is_array($blockIds)) {
-            $blockIds = explode(',', (string) $blockIds);
-        }
-
-        $adapter = $this->_getWriteAdapter();
-        $condition = sprintf(
-            '(%s) AND (%s)',
-            $adapter->prepareSqlCondition('request_id', $request->getId()),
-            $adapter->prepareSqlCondition('block_id', array('nin' => $blockIds)));
-        $adapter->delete($blockTable, $condition);
-
-        $insertData = array();
-        foreach ($blockIds as $blockId) {
-            $insertData[] = array(
-                'request_id' => $request->getId(),
-                'block_id' => $blockId
-            );
-        }
-        $adapter->insertOnDuplicate($blockTable, $insertData);
-        $request->setOrigData('block_ids', $blockIds);
-    }
-
-    protected function _savePages(Capita_TI_Model_Request $request)
-    {
-        $pageTable = $this->getTable('capita_ti/page');
-        $pageIds = $request->getPageIds();
-        if (!is_array($pageIds)) {
-            $pageIds = explode(',', (string) $pageIds);
-        }
-
-        $adapter = $this->_getWriteAdapter();
-        $condition = sprintf(
-            '(%s) AND (%s)',
-            $adapter->prepareSqlCondition('request_id', $request->getId()),
-            $adapter->prepareSqlCondition('page_id', array('nin' => $pageIds)));
-        $adapter->delete($pageTable, $condition);
-
-        $insertData = array();
-        foreach ($pageIds as $pageId) {
-            $insertData[] = array(
-                'request_id' => $request->getId(),
-                'page_id' => $pageId
-            );
-        }
-        $adapter->insertOnDuplicate($pageTable, $insertData);
-        $request->setOrigData('page_ids', $pageIds);
-    }
-
-    protected function _saveAttributes(Capita_TI_Model_Request $request)
-    {
-        $attributeTable = $this->getTable('capita_ti/attribute');
-        $attributeIds = $request->getAttributeIds();
-        if (!is_array($attributeIds)) {
-            $attributeIds = explode(',', (string) $attributeIds);
-        }
-
-        $adapter = $this->_getWriteAdapter();
-        $condition = sprintf(
-            '(%s) AND (%s)',
-            $adapter->prepareSqlCondition('request_id', $request->getId()),
-            $adapter->prepareSqlCondition('attribute_id', array('nin' => $attributeIds)));
-        $adapter->delete($attributeTable, $condition);
-
-        $insertData = array();
-        foreach ($attributeIds as $attributeId) {
-            $insertData[] = array(
-                'request_id' => $request->getId(),
-                'attribute_id' => $attributeId
-            );
-        }
-        $adapter->insertOnDuplicate($attributeTable, $insertData);
-        $request->setOrigData('attribute_ids', $attributeIds);
+        $adapter->insertOnDuplicate($linkTable, $insertData);
+        $request->setOrigData($idsFieldName, $entityIds);
     }
 }
